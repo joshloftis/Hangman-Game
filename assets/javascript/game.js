723a78f71aa24e1d6cc8d8e2f32de80f697e9725
Josh Loftis <jshlfts32@gmail.com>
2017-08-24 23:34:16 -0400

    Updating JS to add dashes for random word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,29 +9,31 @@ var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV";
 var guesses = document.getElementById('guesses-remaining');
 var wrongLetters = document.getElementById('letters-guessed');
 
-//Randomly select word from gameWords
-var selectedWord = gameWords[Math.floor(Math.random() * gameWords.length)];
+var selectedWord = gameWords[Math.floor(Math.random() * gameWords.length)];//Randomly select word from gameWords
 console.log(selectedWord);
 
-//User enters a letter
-document.onkeyup = function(event) {
-  var typedLetter = event.key;
-  //print to the screen only *letters* the user types
-  if (alphabet.indexOf(typedLetter) > -1) {
-    //add typed *letter* to userGuesses array
-    userGuesses.push(typedLetter);
-    // If the letter typed is less than the 0 index of selectedWord (not a letter in the word),
-    //then the remainingGuesses is decreased by 1, the new remainingGuesses value run, and
-    //the incorrect letters are logged successively to the page
-    if (selectedWord.indexOf(typedLetter) < 0) {
-      remainingGuesses--;
-      guesses.innerHTML = remainingGuesses;
-      wrongLetters.textContent += typedLetter + " ";
-    } else {
-    }
+function dashes() {
+  for (var i = 0; i < selectedWord.length; i++) {
+    var currentWordSpan = document.getElementById('currentWord');
+    currentWordSpan.innerHTML += "_";
   }
-  //if remainingGuesses reaches 0 then an alert is run, and then reset the reset game function
-  if (remainingGuesses === 0) {
+}
+
+dashes();
+
+document.onkeyup = function(event) {
+  var typedLetter = event.key; //User enters a letter
+  if (alphabet.indexOf(typedLetter) > -1) { //print to the screen only *letters* the user types
+    userGuesses.push(typedLetter); //add typed *letter* to userGuesses array
+    if (selectedWord.indexOf(typedLetter) < 0) { //If the user typed letter isn't a letter in the randomly selected word, then...
+      remainingGuesses--; //Reduce remainingGuess by 1
+      guesses.innerHTML = remainingGuesses; //Print new remainingGuesses count to div
+      wrongLetters.textContent += typedLetter + " "; //Add the incorrect letters to the div
+    } else if (selectedWord.indexOf(typedLetter) >= 0){
+        console.log(typedLetter);
+      }
+    }
+  if (remainingGuesses === 0) {   //if remainingGuesses reaches 0 then an alert is run, and then reset the reset game function
     alert("YOU LOSE! The correct word, since you couldn't guess it, was " + selectedWord + ". Press \"OK\" to start again.");
     //resets game
   }
